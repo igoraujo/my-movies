a47a9b10cbafe5a1ad28d97c6e53b7c206076046
Igor Araujo <igor@moviy.com.br>
2021-04-19 21:15:28 -0300

    feat: alter movie items and movie list

--- src/api/MoviesService.js
+++ src/api/MoviesService.js
@@ -8,4 +8,8 @@ export class MovieService {
     static getMovies() {
         return axios(withBaseUrl('movie/popular'));
     }
+
+    static getMivieById(id) {
+        return axios(withBaseUrl(`movie/${id}`));
+    }
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -3,7 +3,9 @@ import { MovieItem } from "./MovieItem";
 export const MovieList = ({ movies }) => (
     <section>
         <ul>
-            { movies.map(m => <MovieItem title={ m.title } />) }
+            { movies.map(m => <MovieItem id={ m.id } title={ m.title } />) }
+            {/* { movies.map(m => <MovieItem { ...m } />) } */}
+            {/* { movies.map(m => <MovieItem movie={ m } />) } */}
         </ul>
     </section>
 );
